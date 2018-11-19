cc18166a3abefd872979c13fdb3e3d2cd47f7295
Josivan Pereira da Silva <josivan.engenharia@gmail.com>
2018-11-19 19:50:30 -0200

    Add files via upload

---
+++ IImplementorElement.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IImplementorElement 
+{
+	
+	void DrawShape (float _x, float _y, float _sz, Color _c);
+
+}
